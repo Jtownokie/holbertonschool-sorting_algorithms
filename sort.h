88f28c648b9fbd1b5f13810bea01187a7460987d
Jtownokie <6673@holbertonstudents.com>
2023-08-14 19:44:51 -0700

    Minor corrections

--- sort.h
+++ sort.h
@@ -27,4 +27,5 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, int low, int high,  size_t size);
 void recursive_sort(int *array, int low, int high, size_t size);
+
 #endif
